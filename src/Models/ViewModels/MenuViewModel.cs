cfde2040870e2d37dfdb4d182701532694146381
mtro0203 <martin.trojan.372@gmail.com>
2019-03-16 17:24:50 +0100

    dokonceny kod z workshopu

---
+++ src/Models/ViewModels/MenuViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dontKillBill
+{
+    public class MenuViewModel
+    {
+        /// <summary>
+        /// start of selected interval
+        /// </summary>
+        [Range(0,int.MaxValue)]
+        [Display(Name ="Zaciatok intervalu")]
+        public int StartPoint { get; set; }
+
+        /// <summary>
+        /// end of selected interval
+        /// </summary>
+        [Range(0, int.MaxValue, ErrorMessage = "Hodnota musi byt kladna")]
+        [Display(Name = "Koniec intervalu")]
+        public int EndPoint { get; set; }
+
+        /// <summary>
+        /// Number of vouchers to be generated
+        /// </summary>
+        [Range(0, int.MaxValue,ErrorMessage ="Hodnota musi byt kladna")]
+        [Display(Name = "Pocet voucherov ktore maju byt vygenerovane")]
+        public int Count { get; set; }
+
+        /// <summary>
+        /// voucher id to be generated to qr code
+        /// </summary>
+        [Range(0, int.MaxValue, ErrorMessage = "Hodnota musi byt kladna")]
+        [Display(Name = "Pocet voucherov ktore maju byt vygenerovane")]
+        public int ToQr { get; set; }
+
+    }
+}
